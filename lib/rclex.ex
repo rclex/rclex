12aec14bda7bd105638e983f1392550f10b2c588
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2019-12-10 18:39:06 +0900

    call init functions

--- lib/rclex.ex
+++ lib/rclex.ex
@@ -1,17 +1,40 @@
-defmodule Rclex do
-  @moduledoc """
-  Documentation for Rclex.
-  """
+defmodule RclEx do
+  @on_load :load_nifs
+    def load_nifs do
+        IO.puts "load_nifs"
+        :erlang.load_nif('../rclex',0)
+    end
+  #return rcl_init_options_t
+  def rcl_get_zero_initialized_init_options do
+    raise "NIF rcl_get_zero_initialized_init_options/0 not implemented"
+  end
+
+  #return rcl_context_t
+  def rcl_get_zero_initialized_context do
+      raise "NIF rcl_get_zero_initialized_context/0 not implemented"
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Rclex.hello()
-      :world
-
+      return {:ok,rcl_ret_t}
+      arguments...(
+          int argc,
+          char const * const * argv,
+          const rcl_init_options_t * options,
+          rcl_context_t * context)
   """
+  def rcl_init(_a,_b,_c,_d) do
+      raise "NIF rcl_init/4 not implemented"
+  end
+
+  @doc """
+      return {:ok,rcl_ret_t}
+      arguments...(
+          rcl_context_t)
+  """        
+  def rcl_shutdown(_a) do
+      raise "NIF rcl_shutdown/1 not implemented" 
+  end
+
   def hello do
     :world
   end
