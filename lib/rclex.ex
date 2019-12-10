4f4a5700de9201095a9134464e91f3e10d7abfb0
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2019-12-10 23:01:35 +0900

    add functions about publisher

--- lib/rclex.ex
+++ lib/rclex.ex
@@ -2,7 +2,7 @@ defmodule RclEx do
   @on_load :load_nifs
     def load_nifs do
         IO.puts "load_nifs"
-        :erlang.load_nif('../rclex',0)
+        :erlang.load_nif('/home/imanishi/rclex/rclex',0)
     end
   #return rcl_init_options_t
   def rcl_get_zero_initialized_init_options do
@@ -77,7 +77,47 @@ defmodule RclEx do
       rcl_node_init(zero_node,'hoge','fuga',con,node_op) |> IO.inspect
       IO.puts "success!"
   end
+#------------------------------publisher_nif.c--------------------------
+  def rcl_get_zero_initialized_publisher do
+      raise "NIF get_zero_initialized_publisher/0 not implemented"
+  end
 
+  def rcl_publisher_get_default_options do
+      raise "rcl_get_zero_initialized_publisher/0 not implemented"
+  end
+
+  def rcl_publisher_get_topic_name(_a) do
+      raise "rcl_get_zero_initialized_publisher/0 not implemented"
+  end
+
+  def rcl_publisher_fini(_a,_b) do
+      raise "rcl_get_zero_initialized_publisher/0 not implemented"
+  end
+
+  def rcl_publisher_init(_a,_b,_c,_d,_e) do
+      raise "rcl_get_zero_initialized_publisher/0 not implemented"
+  end
+
+  def rcl_publisher_is_valid(_a) do
+      raise "rcl_get_zero_initialized_publisher/0 not implemented"
+  end
+  def rcl_publish(_a,_b,_c) do
+      raise "rcl_publish/3 not implemented"
+  end
+
+  def setting_pub do
+    con = rcl_get_zero_initialized_context()
+    op = rcl_get_zero_initialized_init_options()
+    rcl_init(0,'hello world',op,con)
+    node_op = rcl_node_get_default_options()
+    node = rcl_get_zero_initialized_node()
+    rcl_node_init(node,'test_node','test_namespace_',con,node_op)
+    mypub = rcl_get_zero_initialized_publisher()
+    mypubop = rcl_publisher_get_default_options()
+    #type_support = Int16.get_message_type_from_std_msgs_msg_Int16
+    #ret = Publisher.rcl_publisher_init(mypub,node,type_support,"topicname",mypubop)
+    IO.puts "success!!"
+  end
   def hello do
     :world
   end
