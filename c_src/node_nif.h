c5f3acfc9e3a78ac81d56701bf203562fe42f4ae
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2019-12-10 18:58:41 +0900

    create_node by rcl_node_init

---
+++ c_src/node_nif.h
@@ -0,0 +1,9 @@
+#include <erl_nif.h>
+
+ERL_NIF_TERM nif_rcl_get_zero_initialized_node(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+
+ERL_NIF_TERM nif_rcl_node_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+
+ERL_NIF_TERM nif_rcl_node_fini(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+
+ERL_NIF_TERM nif_rcl_node_get_default_options(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
