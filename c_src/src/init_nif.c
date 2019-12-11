c88ec1aed137081c94e0c8f4fd735bfa40615914
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2019-12-11 16:57:01 +0900

    remove printfs for debugging

--- c_src/src/init_nif.c
+++ c_src/src/init_nif.c
@@ -12,12 +12,9 @@ extern "C"
 
 
 ERL_NIF_TERM nif_rcl_get_zero_initialized_init_options(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-    printf("op1\n");
     if(argc != 0){
-        printf("op2\n");
         return enif_make_badarg(env);
     }
-    printf("op3\n");
     rcl_init_options_t* res;
     ERL_NIF_TERM ret;
     res = enif_alloc_resource(rt_init_options,sizeof(rcl_init_options_t));
@@ -47,9 +44,8 @@ ERL_NIF_TERM nif_rcl_get_zero_initialized_context(ErlNifEnv* env, int argc, cons
 }
 
 ERL_NIF_TERM nif_rcl_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-    printf("ini1\n");
+    
     if(argc != 4){
-        printf("ini2\n");
         return enif_make_badarg(env);
     }
 
@@ -58,25 +54,20 @@ ERL_NIF_TERM nif_rcl_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
     int argcval;
     char argv_buf[1024];    
     (void)memset(&argv_buf, '\0', sizeof(argv_buf));
-    printf("ini3\n");
     const rcl_init_options_t* res_arg_options;
     rcl_context_t* res_arg_context;
     if(!enif_get_int(env,argv[0],&argcval)){
         return enif_make_badarg(env);
     }
-    printf("ini4\n");
     if(!enif_get_string(env,argv[1],argv_buf,sizeof(argv_buf),ERL_NIF_LATIN1)){
         return enif_make_badarg(env);
     }
-    printf("ini5\n");
     if(!enif_get_resource(env, argv[2], rt_init_options, (void**) &res_arg_options)){
 	    return enif_make_badarg(env);
     }
-    printf("ini6\n");
     if(!enif_get_resource(env, argv[3], rt_context, (void**) &res_arg_context)){
 	    return enif_make_badarg(env);
     }
-    printf("ini7\n");
     res = enif_alloc_resource(rt_ret,sizeof(rcl_ret_t));
     if(res == NULL) return enif_make_badarg(env);
     ret = enif_make_resource(env,res);
