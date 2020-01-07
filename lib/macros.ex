88d1421bad6688ae725ee5a5ad5b111ea3b02360
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2020-01-07 16:19:07 +0900

    for judging return value

---
+++ lib/macros.ex
@@ -0,0 +1,8 @@
+defmodule RclEx.Macros do
+  defmacro rcl_ret_ok, do: 0
+  defmacro rcl_ret_error, do: 1
+  defmacro rcl_ret_timeout, do: 2
+  defmacro rcl_ret_bad_alloc, do: 10
+  defmacro rmw_ret_invalid_argument, do: 11
+  defmacro rcl_ret_subscription_take_failed, do: 401
+end
