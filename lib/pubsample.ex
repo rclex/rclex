e05487a1613f4225fdf9fea5e10014bc08eca416
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2020-01-07 17:22:49 +0900

    sample for how to create publisher

---
+++ lib/pubsample.ex
@@ -0,0 +1,28 @@
+defmodule PubSample do
+
+  def pubmain do
+    init_op_pub = RclEx.rcl_get_zero_initialized_init_options()
+    #RclEx.rcl_init_options_init(init_op_sub)
+    pub_context = RclEx.rcl_get_zero_initialized_context()
+    #RclEx.rcl_init_with_null(init_op_sub,sub_context)
+    #RclEx.rcl_init_options_fini(init_op_sub)
+    
+    RclEx.rclexinit(init_op_pub,pub_context)
+    
+    pubnode = RclEx.rcl_get_zero_initialized_node()
+    pubnode_op = RclEx.rcl_node_get_default_options()
+    RclEx.rcl_node_init(pubnode,'test_pub_node','test_pub_namespace_',pub_context,pubnode_op)
+    pub = RclEx.rcl_get_zero_initialized_publisher()
+    pub_op = RclEx.rcl_publisher_get_default_options()
+
+    RclEx.rcl_publisher_init(pub,pubnode,'testtopic',pub_op)
+    pub_alloc = RclEx.create_pub_alloc()
+    pubmsg = RclEx.create_empty_msgInt16()
+    RclEx.set_data(pubmsg,1)
+
+    #RclEx.rcl_publish(pub,pubmsg,pub_alloc)
+
+    publisher_info = RclEx.Spin.publisher_start(pubmsg,pub,pub_alloc)
+    
+  end
+end
