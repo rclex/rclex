aef4fb093349f8f6fee9ce79435ea70df5faf038
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2020-01-08 20:10:13 +0900

    print_msg changes to read_data

--- c_src/include/msg/msg_int16_nif.h
+++ c_src/include/msg/msg_int16_nif.h
@@ -10,6 +10,6 @@ ERL_NIF_TERM nif_std_msgs__msg__Int16__destroy(ErlNifEnv* env, int argc, const E
 
 ERL_NIF_TERM nif_getmsgtype_int16(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
-ERL_NIF_TERM nif_print_msg(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM nif_read_data(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
 ERL_NIF_TERM nif_set_data(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
