03e56e4f76032b23ab45a98de9d8b2d8ebbfc439
HiroiImanishi <imanishi@lab3.kuis.kyoto-u.ac.jp>
2020-01-18 17:59:28 +0900

    correct publisher about namespace, spin task

--- lib/spin.ex
+++ lib/spin.ex
@@ -3,32 +3,39 @@ defmodule RclEx.Spin do
   require IEx
 
   #pubtaskspinとpubtaskspinはパターンマッチで選べるようにできれば...
+  def singlepublisher_spin(pub,pubmsg,callback) do
+    pub_alloc = RclEx.create_pub_alloc()
+    pub_pid = Task.async(fn -> pub_spin(pub,pubmsg,pub_alloc,callback) end)
+  end
 
-  def publoop(pub,pubmsg,pub_alloc,callback) do
+  def pub_spin_once(pub,pubmsg,pub_alloc,callback) do
     case RclEx.rcl_publish(pub,pubmsg,pub_alloc) do
       {RclEx.Macros.rcl_ret_ok,_,_} -> callback.(pubmsg)
       {RclEx.Macros.rcl_ret_publisher_invalid,_,_} -> IO.puts "Publisher is invalid"
       {RclEx.Macros.rmw_ret_invalid_argument,_,_} -> IO.puts "invalid argument is contained"
       {RclEx.Macros.rcl_ret_error,_,_} -> IO.puts "unspecified error"
-      {_,_,_} -> IO.puts "What!?"
+      {_,_,_} -> do_nothing()
     end
     :timer.sleep(2000)
-    publoop(pub,pubmsg,pub_alloc,callback)
+  end
+  
+  def pub_spin(pub,pubmsg,pub_alloc,callback) do
+    pub_spin_once(pub,pubmsg,pub_alloc,callback)
+    pub_spin(pub,pubmsg,pub_alloc,callback)
   end
 
-  def singlepublisher_spin(pub,pubmsg,callback) do
-    pub_alloc = RclEx.create_pub_alloc()
-    pub_pid = Task.async(fn -> publoop(pub,pubmsg,pub_alloc,callback) end)
-  end
-
-  def publisher_spin(publisher_list,pubmsg_list,callback) do
-    Enum.map(0..length(publisher_list)-1,fn(index)-> 
-      Task.async(fn -> 
-        publoop(Enum.at(publisher_list,index),Enum.at(pubmsg_list,index),RclEx.create_pub_alloc(),callback) 
-      end)  
+  def pub_task_start(publisher_list,pubmsg_list,callback) do
+    {:ok,supervisor} = Task.Supervisor.start_link()
+    Enum.map(0..length(publisher_list)-1,fn(index)->
+      #  Task.async(fn -> sub_spin(RclEx.create_empty_msgInt16(),subscriber,RclEx.create_msginfo(),RclEx.create_sub_alloc(),callback) end)
+         Task.Supervisor.start_child(supervisor,RclEx.Spin,:pub_spin,
+         [Enum.at(publisher_list,index),Enum.at(pubmsg_list,index),RclEx.create_pub_alloc(),callback],
+         [restart: :transient])  
+      #Task.async(fn -> 
+        #publoop(Enum.at(publisher_list,index),Enum.at(pubmsg_list,index),RclEx.create_pub_alloc(),callback)  
     end)
   end
-
+  
   defp do_nothing do
     #noop
   end
