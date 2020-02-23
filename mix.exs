21975b56a72cf108058bf76a424d37738925094a
takasehideki <takase@i.kyoto-u.ac.jp>
2020-02-23 20:27:13 +0900

    apply mix format

--- mix.exs
+++ mix.exs
@@ -17,18 +17,19 @@ defmodule Rclex.MixProject do
       deps: deps(),
       compilers: [:elixir_make] ++ Mix.compilers(),
       elixirc_paths: elixirc_paths(Mix.env()),
-      make_makefile: "c_src/Makefile",
+      make_makefile: "c_src/Makefile"
     ]
   end
 
   defp package do
     [
       name: "rclex",
-      maintainers: ["hiroiimanishi","takasehideki"],
+      maintainers: ["hiroiimanishi", "takasehideki"],
       licenses: ["Apache-2.0"],
       links: %{"Github" => "https://github.com/tlk-emb/rclex"}
     ]
   end
+
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
@@ -40,12 +41,11 @@ defmodule Rclex.MixProject do
   defp deps do
     [
       {:elixir_make, "~> 0.4", runtime: false},
-      {:timex,"~>3.5"}
+      {:timex, "~>3.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
   defp elixirc_paths(_), do: ["lib", "c_src"]
-
 end
