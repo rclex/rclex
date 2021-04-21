690ea840b873693267ffeee6e65e23e18d524f14
kebus426 <kemu423@gmail.com>
2021-04-22 00:16:50 +0900

    puts text

--- test/rclex_sample_test.exs
+++ test/rclex_sample_test.exs
@@ -2,6 +2,7 @@ defmodule RclexSamplesTest do
     use ExUnit.Case
   
     test "run RclexSamples in rclex repository" do
+      IO.puts(RclexSamples.hello())
       assert RclexSamples.hello() == :world
     end
   end
