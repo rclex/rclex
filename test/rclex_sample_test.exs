e8bfb5a85cae397ea475ccef5c4381b0cf52603e
kebus426 <kemu423@gmail.com>
2021-05-03 02:05:02 +0900

    remove rclex_samples_test.exs

--- test/rclex_sample_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RclexSamplesTest do
-    use ExUnit.Case
-  
-    test "run RclexSamples in rclex repository" do
-      IO.puts(RclexSamples.hello())
-      assert RclexSamples.hello() == :world
-    end
-  end
