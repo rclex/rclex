be08a56610d7ce2ca849b278a4a9a2b4b84a4d0d
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-07-12 21:50:21 +0900

    apply astyle to src/*.c

--- src/graph_nif.c
+++ src/graph_nif.c
@@ -19,64 +19,67 @@ rcl_get_topic_names_and_types(
    bool no_demangle,
    rcl_names_and_types_t * topic_names_and_types);
 */
-ERL_NIF_TERM nif_rcl_get_topic_names_and_types(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-    if(argc != 3){
-        printf("arg tarite naiyo\n");
-        return enif_make_badarg(env);
-    }
-    int res;
-    rcl_names_and_types_t * res_names_and_types;
-    ERL_NIF_TERM ret;
+ERL_NIF_TERM nif_rcl_get_topic_names_and_types(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  if(argc != 3) {
+    printf("arg tarite naiyo\n");
+    return enif_make_badarg(env);
+  }
+  int res;
+  rcl_names_and_types_t * res_names_and_types;
+  ERL_NIF_TERM ret;
 
-    rcl_node_t* res_arg_node;
-    rcl_allocator_t* res_alloc;
-    char res_no_demangle[128];
-    
-    (void)memset(&res_no_demangle,'\0',sizeof(res_no_demangle));
-   
-    if(!enif_get_resource(env, argv[0], rt_node, (void**) &res_arg_node)){
-        return enif_make_badarg(env);
-    }
+  rcl_node_t* res_arg_node;
+  rcl_allocator_t* res_alloc;
+  char res_no_demangle[128];
 
-    if(!enif_get_resource(env, argv[1], rt_default_alloc, (void**) &res_alloc)){
-        return enif_make_badarg(env);
-    }
+  (void)memset(&res_no_demangle,'\0',sizeof(res_no_demangle));
 
-    if(!enif_get_atom(env, argv[2], res_no_demangle, sizeof(res_no_demangle),ERL_NIF_LATIN1)){
-        return enif_make_badarg(env);
-    }
-    bool no_demangle = res_no_demangle == "true";
-    
-    res_names_and_types = enif_alloc_resource(rt_names_and_types,sizeof(rcl_names_and_types_t));
-    if(res_names_and_types == NULL) return enif_make_badarg(env);
-    ret = enif_make_resource(env,res_names_and_types);
-    (void)memset(&res_names_and_types[0],NULL,sizeof(rcl_names_and_types_t));
-    res = rcl_get_topic_names_and_types(res_arg_node, res_alloc, no_demangle, res_names_and_types);
+  if(!enif_get_resource(env, argv[0], rt_node, (void**) &res_arg_node)) {
+    return enif_make_badarg(env);
+  }
 
-    int names_length = res_names_and_types->names.size;
-    ERL_NIF_TERM *names_and_types_array = enif_alloc(sizeof(ERL_NIF_TERM) * names_length);
-    for(int i = 0; i < names_length; i++) {
-        int types_length = res_names_and_types->types[i].size;
-        ERL_NIF_TERM *types_array = enif_alloc(sizeof(ERL_NIF_TERM) * types_length);
-        for(int j = 0; j < types_length; j++){
-            types_array[j] = enif_make_string(env, res_names_and_types->types[i].data[j], ERL_NIF_LATIN1);
-        }
-        names_and_types_array[i] = enif_make_tuple2(
-            env,
-            enif_make_string(env, res_names_and_types->names.data[i], ERL_NIF_LATIN1),
-            enif_make_list_from_array(
-                env, 
-                types_array,
-                types_length
-            )
-        );
-    }
+  if(!enif_get_resource(env, argv[1], rt_default_alloc, (void**) &res_alloc)) {
+    return enif_make_badarg(env);
+  }
 
-    return enif_make_list_from_array(
-        env,
-        names_and_types_array,
-        names_length
-    );
+  if(!enif_get_atom(env, argv[2], res_no_demangle, sizeof(res_no_demangle),ERL_NIF_LATIN1)) {
+    return enif_make_badarg(env);
+  }
+  bool no_demangle = res_no_demangle == "true";
+
+  res_names_and_types = enif_alloc_resource(rt_names_and_types,sizeof(rcl_names_and_types_t));
+  if(res_names_and_types == NULL) {
+    return enif_make_badarg(env);
+  }
+  ret = enif_make_resource(env,res_names_and_types);
+  (void)memset(&res_names_and_types[0],NULL,sizeof(rcl_names_and_types_t));
+  res = rcl_get_topic_names_and_types(res_arg_node, res_alloc, no_demangle, res_names_and_types);
+
+  int names_length = res_names_and_types->names.size;
+  ERL_NIF_TERM *names_and_types_array = enif_alloc(sizeof(ERL_NIF_TERM) * names_length);
+  for(int i = 0; i < names_length; i++) {
+    int types_length = res_names_and_types->types[i].size;
+    ERL_NIF_TERM *types_array = enif_alloc(sizeof(ERL_NIF_TERM) * types_length);
+    for(int j = 0; j < types_length; j++) {
+      types_array[j] = enif_make_string(env, res_names_and_types->types[i].data[j], ERL_NIF_LATIN1);
+    }
+    names_and_types_array[i] = enif_make_tuple2(
+                                 env,
+                                 enif_make_string(env, res_names_and_types->names.data[i], ERL_NIF_LATIN1),
+                                 enif_make_list_from_array(
+                                   env,
+                                   types_array,
+                                   types_length
+                                 )
+                               );
+  }
+
+  return enif_make_list_from_array(
+           env,
+           names_and_types_array,
+           names_length
+         );
 }
 
 #ifdef __cplusplus
--- src/msg_string_nif.c
+++ src/msg_string_nif.c
@@ -21,30 +21,34 @@ extern "C"
 
 
 //空のStringメッセージオブジェクトを作る関数
-ERL_NIF_TERM nif_create_empty_string(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-    if(argc != 0){
-        return enif_make_badarg(env);
-    }
-    std_msgs__msg__String* res;
-    ERL_NIF_TERM ret;
-    res = enif_alloc_resource(rt_String,sizeof(std_msgs__msg__String));
-    if(res == NULL) return enif_make_badarg(env);
-    ret = enif_make_resource(env,res);
+ERL_NIF_TERM nif_create_empty_string(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  if(argc != 0) {
+    return enif_make_badarg(env);
+  }
+  std_msgs__msg__String* res;
+  ERL_NIF_TERM ret;
+  res = enif_alloc_resource(rt_String,sizeof(std_msgs__msg__String));
+  if(res == NULL) {
+    return enif_make_badarg(env);
+  }
+  ret = enif_make_resource(env,res);
 
-    return ret;
+  return ret;
 }
 
 //init関数
-ERL_NIF_TERM nif_string_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-  if(argc != 1){
+ERL_NIF_TERM nif_string_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  if(argc != 1) {
     return enif_make_badarg(env);
   }
   ERL_NIF_TERM ret;
   std_msgs__msg__String* res_msg;
-  if(!enif_get_resource(env,argv[0],rt_String,(void**)&res_msg)){
-      return enif_make_badarg(env);
+  if(!enif_get_resource(env,argv[0],rt_String,(void**)&res_msg)) {
+    return enif_make_badarg(env);
   }
-  
+
   std_msgs__msg__String__init(res_msg);
   ret = enif_make_resource(env,res_msg);
   return ret;
@@ -52,37 +56,39 @@ ERL_NIF_TERM nif_string_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]
 }
 
 //std_msgs__msg__Stringのdataに文字列を入れる関数
-ERL_NIF_TERM nif_setdata_string(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-    if(argc != 3){
-        return enif_make_badarg(env);
-    }
-    std_msgs__msg__String* res_msg;
-    ERL_NIF_TERM ret;
-    int str_size = 0;
-    if(!enif_get_resource(env,argv[0],rt_String,(void**)&res_msg)){
-        return enif_make_badarg(env);
-    }
-    if(!enif_get_int(env,argv[2],&str_size)){
-        return enif_make_badarg(env);
-    }
-    char* data_buf = (char*) malloc(str_size);   //この値がデータサイズの上限を変更する
-    if(!enif_get_string(env,argv[1],data_buf,str_size,ERL_NIF_LATIN1)){
-        return enif_make_badarg(env);
-    }
-    //String型の構造体に引数の文字列とサイズを入れる．
-    __STRING__ASSIGN(res_msg,data_buf);
-    free(data_buf);
-    return enif_make_atom(env,"ok");
+ERL_NIF_TERM nif_setdata_string(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  if(argc != 3) {
+    return enif_make_badarg(env);
+  }
+  std_msgs__msg__String* res_msg;
+  ERL_NIF_TERM ret;
+  int str_size = 0;
+  if(!enif_get_resource(env,argv[0],rt_String,(void**)&res_msg)) {
+    return enif_make_badarg(env);
+  }
+  if(!enif_get_int(env,argv[2],&str_size)) {
+    return enif_make_badarg(env);
+  }
+  char* data_buf = (char*) malloc(str_size);   //この値がデータサイズの上限を変更する
+  if(!enif_get_string(env,argv[1],data_buf,str_size,ERL_NIF_LATIN1)) {
+    return enif_make_badarg(env);
+  }
+  //String型の構造体に引数の文字列とサイズを入れる．
+  __STRING__ASSIGN(res_msg,data_buf);
+  free(data_buf);
+  return enif_make_atom(env,"ok");
 }
-ERL_NIF_TERM nif_readdata_string(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]){
-    if(argc != 1){
-        return enif_make_badarg(env);
-    }
-    std_msgs__msg__String* res_msg;
-    if(!enif_get_resource(env,argv[0],rt_String,(void**)&res_msg)){
-        return enif_make_badarg(env);
-    }
-    return enif_make_string(env,res_msg->data.data,ERL_NIF_LATIN1);
+ERL_NIF_TERM nif_readdata_string(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  if(argc != 1) {
+    return enif_make_badarg(env);
+  }
+  std_msgs__msg__String* res_msg;
+  if(!enif_get_resource(env,argv[0],rt_String,(void**)&res_msg)) {
+    return enif_make_badarg(env);
+  }
+  return enif_make_string(env,res_msg->data.data,ERL_NIF_LATIN1);
 }
 #ifdef __cplusplus
 }
