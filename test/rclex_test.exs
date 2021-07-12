573481479744d25434082889eed532012409aad3
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-07-12 20:47:34 +0900

    use List.last/1 to obtain the proper info (#43)
    https://hexdocs.pm/elixir/1.11/List.html#last/1

--- test/rclex_test.exs
+++ test/rclex_test.exs
@@ -18,8 +18,8 @@ defmodule RclexTest do
   # -----------------------graph_nif.c--------------------------
   test "rcl_get_topic_names_and_types" do
     context = Rclex.rclexinit()
-    node_list = Rclex.create_nodes(context, 'test_pub_node', 1)
 
+    node_list = Rclex.create_nodes(context, 'test_pub_node', 1)
     publisher_list = Rclex.create_publishers(node_list, 'testtopic', :single)
     node_list_2 = Rclex.create_nodes(context, 'test_sub_node', 1)
     subscriber_list = Rclex.create_subscribers(node_list_2, 'testtopic', :single)
@@ -28,21 +28,14 @@ defmodule RclexTest do
     names_and_types_tuple_list =
       Rclex.rcl_get_topic_names_and_types(node, Rclex.rcl_get_default_allocator(), false)
 
-    [name_and_types_tuple_1 | [name_and_types_tuple_2]] = names_and_types_tuple_list
+    name_and_types_tuple = List.last(names_and_types_tuple_list)
 
-    name_1 = elem(name_and_types_tuple_1, 0)
-    types_list_1 = elem(name_and_types_tuple_1, 1)
+    name = elem(name_and_types_tuple, 0)
+    types_list = elem(name_and_types_tuple, 1)
 
-    assert name_1 == '/rosout', 'first topic name is rosout'
-    type_1 = hd(types_list_1)
-    assert type_1 == 'rcl_interfaces/msg/Log', 'first topic type is rcl_interfaces/msg/Log'
-
-    name_2 = elem(name_and_types_tuple_2, 0)
-    types_list_2 = elem(name_and_types_tuple_2, 1)
-
-    assert name_2 == '/testtopic', 'second topic name is testtopic'
-    type_2 = hd(types_list_2)
-    assert type_2 == 'std_msgs/msg/String', 'second topic type is std_msgs/msg/String'
+    assert name == '/testtopic', 'topic name is testtopic'
+    type = hd(types_list)
+    assert type == 'std_msgs/msg/String', 'topic type is std_msgs/msg/String'
 
     Rclex.publisher_finish(publisher_list, node_list)
     Rclex.subscriber_finish(subscriber_list, node_list_2)
