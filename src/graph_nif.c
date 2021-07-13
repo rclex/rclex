f97f1c841cf8423e3af6b05b08e367262b5aaea9
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-07-13 15:48:48 +0900

    remove unnecessory printf

--- src/graph_nif.c
+++ src/graph_nif.c
@@ -22,7 +22,6 @@ rcl_get_topic_names_and_types(
 ERL_NIF_TERM nif_rcl_get_topic_names_and_types(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
   if(argc != 3) {
-    printf("arg tarite naiyo\n");
     return enif_make_badarg(env);
   }
   int res;
