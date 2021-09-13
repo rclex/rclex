48147caad09181e8e9b460416ff5a9b9340ddae8
kebus426 <kemu423@gmail.com>
2021-09-13 12:30:34 +0900

    末尾再帰に変更

---
+++ lib/rclex/loop.ex
@@ -0,0 +1,90 @@
+defmodule Rclex.Loop do
+    alias Rclex.Nifs
+    require Rclex.Macros
+    require Logger
+    use GenServer
+
+    def init_sub(sub, context, call_back) do
+        GenServer.start_link(__MODULE__, {sub, context, call_back})
+    end
+
+    def init_pub(sub, context, call_back) do
+        {:ok, pid} = GenServer.start_link(__MODULE__, {sub, context, call_back})
+    end
+
+    def init({sub, context, call_back}) do
+      wait_set = Nifs.rcl_get_zero_initialized_wait_set()
+      |> Nifs.rcl_wait_set_init(
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        context,
+        Nifs.rcl_get_default_allocator()
+      )
+        {:ok, {wait_set, sub, call_back}} 
+    end
+
+    def start_sub(id_list) do
+        id_list
+        |> Enum.map(fn pid -> GenServer.cast(pid, {:loop}) end)
+    end
+
+    @doc """
+        購読処理関数
+        購読が正常に行われれば，引数に受け取っていたコールバック関数を実行
+    """
+    def each_subscribe(sub, call_back) do
+        if Nifs.check_subscription(sub) do
+            msg = Rclex.initialize_msg()
+            msginfo = Nifs.create_msginfo()
+            sub_alloc = Nifs.create_sub_alloc()
+
+            case Nifs.rcl_take(sub, msg, msginfo, sub_alloc) do
+                {Rclex.Macros.rcl_ret_ok(), _, _, _} ->
+                call_back.(msg)
+
+                {Rclex.Macros.rcl_ret_subscription_invalid(), _, _, _} ->
+                Logger.error("subscription invalid")
+
+                {Rclex.Macros.rcl_ret_subscription_take_failed(), _, _, _} ->
+                do_nothing
+            end
+        end
+    end
+
+    def handle_cast({:loop}, {wait_set, sub, call_back}) do
+        {:noreply, {wait_set, sub, call_back}, {:continue, :loop} }
+    end
+
+    def handle_continue(:loop, {wait_set, sub, call_back}) do
+        Nifs.rcl_wait_set_clear(wait_set)
+        # waitsetにサブスクライバを追加する
+        Nifs.rcl_wait_set_add_subscription(wait_set, sub)
+
+        # wait_setからsubのリストを取りだす
+        [waitset_sub]= Nifs.get_sublist_from_waitset(wait_set)
+
+        # 待機時間によってCPU使用率，購読までの時間は変わる
+        Nifs.rcl_wait(wait_set, 20)
+
+        # # 購読タスク達のスーパーバイザを作成
+        # {:ok, sv} = Task.Supervisor.start_link()
+
+        # # 購読タスクをサブスクライバの数だけ生成，each_subscribe/2を実行させる．
+        # Enum.map(waitset_sublist, fn sub ->
+        # Task.Supervisor.start_child(sv, Rclex.Subscriber, :each_subscribe, [sub, call_back],
+        #     restart: :transient
+        # )
+        # end)
+
+        each_subscribe(waitset_sub, call_back)
+
+        {:noreply, {wait_set, sub, call_back}, {:continue, :loop}}
+    end
+
+    def do_nothing() do
+    end
+end
