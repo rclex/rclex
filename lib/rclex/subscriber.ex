b57725b96f3ffa9db12604b5d0e28c34f4e301b0
kebus426 <kemu423@gmail.com>
2021-10-20 04:43:15 +0900

    subscirber.exのコメント等追加

--- lib/rclex/subscriber.ex
+++ lib/rclex/subscriber.ex
@@ -8,52 +8,33 @@ defmodule Rclex.Subscriber do
   T.B.A
   """
 
-  ## TODO: ここでsubscriberを生成、トピック名を覚えておく
-
-  # @doc """
-  #   subscriberプロセスの生成
-  # """
-  # def start_link(sub, context, call_back) do
-  #   Logger.debug("sub link start")
-  #   GenServer.start_link(__MODULE__, {sub, context, call_back}, name: )
-  # end
-
   @doc """
     subscriberプロセスの生成
   """
   def start_link({sub, process_name}) do
-    Logger.debug("sub link start")
+    Logger.debug("#{process_name} subscriber process start")
     GenServer.start_link(__MODULE__, sub, name: {:global, process_name})
   end
 
-  # @doc """
-  #   subscriberプロセスの初期化
-  #   subscriberとコールバック関数を状態として持つ。
-  #   同時に購読を開始する
-  # """
-  # def init({sub, context, call_back}) do
-  #   children = [
-  #     {Rclex.Loop, {self(), sub, context, call_back}}
-  #   ]
-  #   opts = [strategy: :one_for_one]
-  #   {:ok, supervisor_id} = Supervisor.start_link(children, opts)
-  #   {:ok, %{subscriber: sub, context: context, call_back: call_back, supervisor_id: supervisor_id}} 
-  # end
-
   @doc """
     subscriberプロセスの初期化
-    subscriberを状態として持つ。
+    subscriberを状態として持つ。start_subscribingをした際にcontextとcall_backを追加で状態として持つ。
   """
   def init(sub) do
     {:ok,  %{subscriber: sub}}
   end
 
-  def subscribe_start({node_identifier, topic_name, :sub}, context, call_back) do
+  def start_subscribing({node_identifier, topic_name, :sub}, context, call_back) do
     sub_identifier = node_identifier ++ '/' ++ topic_name
-    GenServer.cast({:global, sub_identifier}, {:subscribe_start, {context, call_back}})
+    GenServer.cast({:global, sub_identifier}, {:start_subscribing, {context, call_back}})
   end
 
-  def handle_cast({:subscribe_start, {context, call_back}}, state) do
+  def stop_subscribing({node_identifier, topic_name, :sub}) do
+    sub_identifier = node_identifier ++ '/' ++ topic_name
+    :ok = GenServer.call({:global, sub_identifier}, :stop_subscribing)
+  end
+
+  def handle_cast({:start_subscribing, {context, call_back}}, state) do
     {:ok, sub} = Map.fetch(state, :subscriber)
     children = [
       {Rclex.SubLoop, {self(), sub, context, call_back}}
@@ -76,19 +57,23 @@ defmodule Rclex.Subscriber do
     {:stop, :normal, state}
   end
 
-  def handle_call(:subscribe_stop, _from, state) do
+  def handle_call(:stop_subscribing, _from, state) do
     {:ok, supervisor_id} = Map.fetch(state, :supervisor_id)
     Supervisor.stop(supervisor_id)
     new_state = Map.delete(state, :supervisor_id)
-    {:reply, {:ok, "stop_loop"}, new_state}
+    {:reply, :ok, new_state}
+  end
+
+  def handle_call({:finish, node}, _from, state) do
+    {:ok, sub} = Map.fetch(state, :subscriber)
+    Nifs.rcl_subscription_fini(sub, node)
+    {:reply, {:ok, 'subscriber finished: '}, state}
   end
 
   def handle_call({:finish_subscriber, node}, _from, state) do
     {:ok, sub} = Map.fetch(state, :subscriber)
     Nifs.rcl_subscription_fini(sub, node)
-    Logger.debug("finish_subscriber")
-    #{:stop, :normal, state}
-    {:reply, {:ok, "subscriber process deleted"}, state}
+    {:reply, :ok, state}
   end
 
   def terminate(:normal, _) do
