80471a94d3294fecd240ee07fe17d1cd9ebffa91
kebus426 <kemu423@gmail.com>
2021-10-20 04:42:49 +0900

    publisher.exのコメント等追加

--- lib/rclex/publisher.ex
+++ lib/rclex/publisher.ex
@@ -11,8 +11,8 @@ defmodule Rclex.Publisher do
   @doc """
     publisherプロセスの生成
   """
-  def start_link(pub) do
-    GenServer.start_link(__MODULE__, pub)
+  def start_link({pub, process_name}) do
+    GenServer.start_link(__MODULE__, pub, name: {:global, process_name})
   end
 
   @doc """
@@ -23,7 +23,6 @@ defmodule Rclex.Publisher do
   end
 
   def publish_once(pub, pubmsg, pub_alloc) do
-    Logger.debug("pubonce")
     case Nifs.rcl_publish(pub, pubmsg, pub_alloc) do
       {Rclex.Macros.rcl_ret_ok(), _, _} ->
         Logger.debug("publish ok")
@@ -42,33 +41,21 @@ defmodule Rclex.Publisher do
     end
   end
 
-  @doc """
-    出版関数
-    スーパーバイザを作成し，パブリッシャの数だけタスクを生成
-    タスクにpublish_onceを実行させる
-  """
-  def publish(publisher_list, pubmsg_list) do
-    {:ok, supervisor} = Task.Supervisor.start_link()
-
-    Enum.map(0..(length(publisher_list) - 1), fn index ->
-      #  Task.async(fn -> sub_spin(Nifs.create_empty_msgInt16(),subscriber,Nifs.create_msginfo(),Nifs.create_sub_alloc(),callback) end)
-      Task.Supervisor.start_child(
-        supervisor,
-        Rclex.Publisher,
-        :publish_once,
-        [Enum.at(publisher_list, index), Enum.at(pubmsg_list, index), Nifs.create_pub_alloc()],
-        restart: :transient
-      )
-
-      # Task.async(fn ->
-      # publoop(Enum.at(publisher_list,index),Enum.at(pubmsg_list,index),Nifs.create_pub_alloc(),callback)
-    end)
+  def publish({node_identifier, topic_name, :pub}, data) do
+      msg = Rclex.initialize_msgs(1, :string)
+      Rclex.setdata(Enum.at(msg, 0), data, :string)
+      key = {:global, node_identifier ++ '/' ++ topic_name}
+      GenServer.cast(JobQueue, {:push, {key, Enum.at(msg, 0)}})
   end
 
-  def handle_cast({:publish, msg}, pub) do
-    Logger.debug("handle_cast publish")
+  def handle_cast({:execute, msg}, pub) do
     Rclex.Publisher.publish_once(pub, msg, Nifs.create_pub_alloc())
     {:noreply, pub}
+  end
+
+  def handle_call({:finish, node}, _from, pub) do
+    Nifs.rcl_publisher_fini(pub, node)
+    {:reply, {:ok, 'publisher finished: '}, pub}
   end
 
   def terminate(:normal, pub) do
