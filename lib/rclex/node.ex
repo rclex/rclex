072470302243963ea42c1affcb0f34d95c73747f
kebus426 <kemu423@gmail.com>
2021-10-20 04:42:27 +0900

    node.exのコメント等追加

--- lib/rclex/node.ex
+++ lib/rclex/node.ex
@@ -4,6 +4,10 @@ defmodule Rclex.Node do
     require Logger
     use GenServer, restart: :transient
 
+@moduledoc """
+  T.B.A
+"""
+
     def start_link({node, node_name}) do
         GenServer.start_link(__MODULE__, {node, node_name}, name: {:global, node_name})
     end
@@ -17,22 +21,35 @@ defmodule Rclex.Node do
         {:ok, {node, name, %{}}}
     end
 
-    def single_create_subscriber(node_identifier, topic_name) do
+    @doc """
+        指定したnodeとtopicのsubscriberプロセスを作成
+        
+    """
+    def create_single_subscriber(node_identifier, topic_name) do
         GenServer.call({:global, node_identifier}, {:create_subscriber, node_identifier, topic_name})
     end
 
-    # def handle_call(:create_publisher, _, state) do
-
-    # end
-
-    def finish_subscriber({sub_node, topic_name, :sub}) do
-        {:ok, _} = GenServer.call({:global, sub_node}, {:finish_subscriber, topic_name})
-        {:ok, "finish_subscribe"}
+    def create_subscribers(node_identifier_list, topic_name) do
+        sub_identifier_list = 
+            Enum.map(node_identifier_list, fn node_identifier -> GenServer.call({:global, node_identifier}, {:create_subscriber, node_identifier, topic_name}) end)
+            |> Enum.map(fn {:ok, sub_identifier} -> sub_identifier end)
+            
+        {:ok, sub_identifier_list}
     end
 
-    def subscribe_stop({sub_node, topic_name, :sub}) do
-        {:ok, _} = GenServer.call({:global, sub_node}, {:subscribe_stop, topic_name})
-        {:ok, "stop subscribe"}
+    def create_single_publisher(node_identifier, topic_name) do
+        GenServer.call({:global, node_identifier}, {:create_publisher, node_identifier, topic_name})
+    end
+
+    def create_publishers(node_identifier_list, topic_name) do
+        pub_identifier_list =  
+            Enum.map(node_identifier_list, fn node_identifier -> GenServer.call({:global, node_identifier}, {:create_publisher, node_identifier, topic_name}) end)
+            |> Enum.map(fn {:ok, pub_identifier} -> pub_identifier end)
+        {:ok, pub_identifier_list}
+    end
+
+    def finish_job({node_identifier, topic_name, role}) do
+        :ok = GenServer.call({:global, node_identifier}, {:finish_job, topic_name, role})
     end
 
     def handle_call({:create_subscriber, node_identifier, topic_name}, _, {node, name, supervisor_ids}) do
@@ -49,39 +66,51 @@ defmodule Rclex.Node do
         {:reply, {:ok, {node_identifier, topic_name, :sub}}, {node, name, new_supervisor_ids}}
     end
 
-    def handle_call({:finish_subscriber, topic_name}, _from, {node, name, supervisor_ids}) do
-        Logger.debug("finish subscriber")
-        # n = length(sub_list)
+    def handle_call({:create_publisher, node_identifier, topic_name}, _, {node, name, supervisor_ids}) do
+        publisher = Nifs.rcl_get_zero_initialized_publisher()
+        pub_op = Nifs.rcl_publisher_get_default_options()
+        pub = Nifs.rcl_publisher_init(publisher, node, topic_name, pub_op)
+        children = [
+            {Rclex.Publisher, {pub, node_identifier ++ '/' ++ topic_name}}
+        ]
+        opts = [strategy: :one_for_one]
+        {:ok, id} = Supervisor.start_link(children, opts)
+        new_supervisor_ids = Map.put_new(supervisor_ids, {:pub, topic_name}, id)
+        Logger.debug(node_identifier ++ '/' ++ topic_name)
+        {:reply, {:ok, {node_identifier, topic_name, :pub}}, {node, name, new_supervisor_ids}}
+    end
 
-        {:ok, supervisor_id} = Map.fetch(supervisor_ids, {:sub, topic_name})
+    def handle_call({:finish_job, topic_name, role}, _from, {node, name, supervisor_ids}) do
+        {:ok, supervisor_id} = Map.fetch(supervisor_ids, {role, topic_name})
 
-        # Enum.map(0..(n - 1), fn index ->
-        # Nifs.rcl_subscription_fini(Enum.at(sub_list, index), Enum.at(node_list, index))
-        # end)
+        key = name ++ '/' ++ topic_name
 
-        sub_key = name ++ '/' ++ topic_name
+        {:ok, text} = GenServer.call({:global, key}, {:finish, node})
 
-        {:ok, text} = GenServer.call({:global, sub_key}, {:finish_subscriber, node})
-
-        Logger.debug(text)
+        Logger.debug(text ++ key)
 
         Supervisor.stop(supervisor_id)
 
         new_supervisor_ids = Map.delete(supervisor_ids, topic_name)
 
-        {:reply, {:ok, "finish supervisor"}, {node, name, new_supervisor_ids}}
+        {:reply, :ok, {node, name, new_supervisor_ids}}
     end
 
-    def handle_call({:subscribe_stop, topic_name}, _from, {node, name, supervisor_ids}) do
-        sub_identifier = name ++ '/' ++ topic_name
-        # replyしないと怒られるのでtextを渡している
-        {:ok, text} = GenServer.call({:global, name ++ '/' ++ topic_name}, :subscribe_stop)
-        Logger.debug(text)
-        {:reply, {:ok, sub_identifier}, {node, name, supervisor_ids}}
+    def handle_call({:finish_subscriber, topic_name}, _from, {node, name, supervisor_ids}) do
+        {:ok, supervisor_id} = Map.fetch(supervisor_ids, {:sub, topic_name})
+
+        sub_key = name ++ '/' ++ topic_name
+
+        :ok = GenServer.call({:global, sub_key}, {:finish_subscriber, node})
+
+        Supervisor.stop(supervisor_id)
+
+        new_supervisor_ids = Map.delete(supervisor_ids, topic_name)
+
+        {:reply, :ok, {node, name, new_supervisor_ids}}
     end
 
     def handle_call(:finish_node, _from, {node, name, supervisor_ids}) do
-        Logger.debug("node finish: #{name}")
         Nifs.rcl_node_fini(node)
 
         #TODO nodeに紐付いているpub,subをきちんと終了させる
