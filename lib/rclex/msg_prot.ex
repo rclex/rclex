4b0be53ae0cbf2340edb94e431698e87fd8fbae1
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-06-30 14:06:34 +0900

    add docs and specs to Rclex.MsgProt protocol

--- lib/rclex/msg_prot.ex
+++ lib/rclex/msg_prot.ex
@@ -1,6 +1,30 @@
 defprotocol Rclex.MsgProt do
+  @moduledoc """
+  Defines protocol which supports arbitrary message types.
+  Modules implementing this protocol should define relationships between Elixir structure data and C structure data.
+  """
+
+  @doc """
+  Should return typesupport reference.
+  """
+  @spec typesupport(msg_type :: struct()) :: reference()
   def typesupport(msg_type)
+
+  @doc """
+  Should return reference to C struct instance.
+  """
+  @spec initialize(msg_type :: struct()) :: reference()
   def initialize(msg_type)
+
+  @doc """
+  Should set Elixir struct to C struct instance.
+  """
+  @spec set(data :: struct(), msg :: reference()) :: :ok
   def set(data, msg)
+
+  @doc """
+  Should return Elixir struct loaded with data from C struct instance.
+  """
+  @spec read(msg_type :: struct(), msg :: reference()) :: struct()
   def read(msg_type, msg)
 end
