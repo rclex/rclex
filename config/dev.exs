754e105f90353009e0ef82ef3ca0e59bd268c653
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-07-07 07:32:52 +0900

    add git_hooks config to config/dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,14 @@
 import Config
+
+config :git_hooks,
+  auto_install: true,
+  verbose: true,
+  # WHY: add -T option, to avoid "the input device is not a TTY" error
+  mix_path: "docker compose run --rm -w /root/rclex -T rclex_docker mix",
+  hooks: [
+    pre_commit: [
+      tasks: [
+        {:cmd, "mix test"}
+      ]
+    ]
+  ]
