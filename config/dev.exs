8c4c7d4aef88ea19f045c85185bfb284d06d8f02
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-07-07 15:00:28 +0900

    change git_hooks config `auto_install` from true to false

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,10 @@
 import Config
 
 config :git_hooks,
-  auto_install: true,
+  # WHY: auto_install: false
+  # Allow developers to choose whether or not to use git_hooks.
+  # If want to use, run `mix git_hooks.install`.
+  auto_install: false,
   verbose: true,
   # WHY: add -T option, to avoid "the input device is not a TTY" error
   mix_path: "docker compose run --rm -w /root/rclex -T rclex_docker mix",
