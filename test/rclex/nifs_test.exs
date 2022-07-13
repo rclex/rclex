66265e75bcc989fa77761931bb60972a0808fee9
pojiro <pojiro.jp@gmail.com>
2022-07-13 11:44:22 +0900

    add unit tests of Rclex.Nifs for init_nif, node_nif, publisher_nif

---
+++ test/rclex/nifs_test.exs
@@ -0,0 +1,214 @@
+defmodule Rclex.NifsTest do
+  use ExUnit.Case
+
+  require Rclex.ReturnCode
+
+  alias Rclex.Nifs
+
+  describe "rcl_get_zero_initialized_init_options/0" do
+    test "return reference" do
+      assert is_reference(Nifs.rcl_get_zero_initialized_init_options())
+    end
+  end
+
+  describe "rcl_init_options_init/1" do
+    test "return :ok" do
+      assert :ok =
+               Nifs.rcl_get_zero_initialized_init_options()
+               |> Nifs.rcl_init_options_init()
+    end
+
+    test "raise ArgumentError" do
+      assert_raise ArgumentError, fn ->
+        make_ref()
+        |> Nifs.rcl_init_options_init()
+      end
+    end
+  end
+
+  describe "rcl_init_options_fini/1" do
+    test "return reference" do
+      options = Nifs.rcl_get_zero_initialized_init_options()
+      :ok = Nifs.rcl_init_options_init(options)
+      assert is_reference(Nifs.rcl_init_options_fini(options))
+    end
+  end
+
+  describe "rcl_get_zero_initialized_context/0" do
+    test "return reference" do
+      assert is_reference(Nifs.rcl_get_zero_initialized_context())
+    end
+  end
+
+  describe "rcl_init_with_null/2" do
+    test "return reference" do
+      init_options = Nifs.rcl_get_zero_initialized_init_options()
+      context = Nifs.rcl_get_zero_initialized_context()
+      assert is_reference(Nifs.rcl_init_with_null(init_options, context))
+    end
+  end
+
+  describe "rcl_shutdown/1" do
+    test "return {:ok, reference}" do
+      context = get_initialized_context()
+
+      assert {:ok, context} = Nifs.rcl_shutdown(context)
+      assert is_reference(context)
+    end
+  end
+
+  describe "rcl_get_zero_initialized_node/0" do
+    test "return reference" do
+      assert is_reference(Nifs.rcl_get_zero_initialized_node())
+    end
+  end
+
+  describe "rcl_node_get_default_options/0" do
+    test "return reference" do
+      assert is_reference(Nifs.rcl_node_get_default_options())
+    end
+  end
+
+  describe "rcl_node_init/5" do
+    test "return reference" do
+      node = Nifs.rcl_get_zero_initialized_node()
+      node_name = 'node'
+      node_namespace = 'namespace'
+      context = get_initialized_context()
+      options = Nifs.rcl_node_get_default_options()
+
+      assert is_reference(Nifs.rcl_node_init(node, node_name, node_namespace, context, options))
+    end
+  end
+
+  describe "rcl_node_init_without_namespace/4" do
+    test "return reference" do
+      node = Nifs.rcl_get_zero_initialized_node()
+      node_name = 'node'
+      context = get_initialized_context()
+      options = Nifs.rcl_node_get_default_options()
+
+      assert is_reference(Nifs.rcl_node_init_without_namespace(node, node_name, context, options))
+    end
+  end
+
+  describe "rcl_node_fini/1" do
+    test "return reference" do
+      node = get_initialized_no_namespace_node()
+      assert is_reference(Nifs.rcl_node_fini(node))
+    end
+  end
+
+  describe "rcl_node_get_name/1" do
+    test "return charlist" do
+      node_name = 'test'
+      node = get_initialized_no_namespace_node(node_name)
+      assert ^node_name = Nifs.rcl_node_get_name(node)
+    end
+  end
+
+  describe "rcl_get_zero_initialized_publisher/0" do
+    test "return reference" do
+      assert is_reference(Nifs.rcl_get_zero_initialized_publisher())
+    end
+  end
+
+  describe "rcl_publisher_get_default_options/0" do
+    test "return reference" do
+      assert is_reference(Nifs.rcl_publisher_get_default_options())
+    end
+  end
+
+  describe "rcl_publisher_fini/2" do
+    test "return reference" do
+      node = get_initialized_no_namespace_node()
+      publihser = get_initialized_publisher(node)
+
+      assert is_reference(Nifs.rcl_publisher_fini(publihser, node))
+    end
+  end
+
+  describe "rcl_publisher_init/5" do
+    test "reutrn reference" do
+      publisher = Nifs.rcl_get_zero_initialized_publisher()
+      node = get_initialized_no_namespace_node()
+      topic = 'topic'
+      typesupport = Rclex.Msg.typesupport('StdMsgs.Msg.String')
+      publisher_options = Nifs.rcl_publisher_get_default_options()
+
+      assert is_reference(
+               Nifs.rcl_publisher_init(publisher, node, topic, typesupport, publisher_options)
+             )
+    end
+  end
+
+  describe "rcl_publisher_is_valid/1" do
+    test "return true" do
+      node = get_initialized_no_namespace_node()
+      publisher = get_initialized_publisher(node)
+
+      assert true = Nifs.rcl_publisher_is_valid(publisher)
+    end
+  end
+
+  describe "rcl_publish/3" do
+    test "" do
+      node = get_initialized_no_namespace_node()
+      publisher = get_initialized_publisher(node)
+      publisher_allocation = Nifs.create_pub_alloc()
+
+      message = Rclex.Msg.initialize('StdMsgs.Msg.String')
+
+      :ok =
+        Rclex.Msg.set(
+          message,
+          %Rclex.StdMsgs.Msg.String{data: 'data'},
+          'StdMsgs.Msg.String'
+        )
+
+      rcl_ret_ok = Rclex.ReturnCode.rcl_ret_ok()
+
+      assert {^rcl_ret_ok, publisher, publisher_allocation} =
+               Nifs.rcl_publish(publisher, message, publisher_allocation)
+
+      assert is_reference(publisher)
+      assert is_reference(publisher_allocation)
+    end
+  end
+
+  describe "create_pub_alloc/0" do
+    test "return reference" do
+      assert is_reference(Nifs.create_pub_alloc())
+    end
+  end
+
+  defp get_initialized_context() do
+    options = Nifs.rcl_get_zero_initialized_init_options()
+    :ok = Nifs.rcl_init_options_init(options)
+    context = Nifs.rcl_get_zero_initialized_context()
+    Nifs.rcl_init_with_null(options, context)
+    Nifs.rcl_init_options_fini(options)
+
+    context
+  end
+
+  defp get_initialized_no_namespace_node(node_name \\ 'node') do
+    node = Nifs.rcl_get_zero_initialized_node()
+    context = get_initialized_context()
+    options = Nifs.rcl_node_get_default_options()
+
+    Nifs.rcl_node_init_without_namespace(node, node_name, context, options)
+  end
+
+  defp get_initialized_publisher(
+         node,
+         topic \\ 'topic',
+         message_type \\ 'StdMsgs.Msg.String',
+         publisher_options \\ Nifs.rcl_publisher_get_default_options()
+       ) do
+    publisher = Nifs.rcl_get_zero_initialized_publisher()
+    typesupport = Rclex.Msg.typesupport(message_type)
+
+    Nifs.rcl_publisher_init(publisher, node, topic, typesupport, publisher_options)
+  end
+end
