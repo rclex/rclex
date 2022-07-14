7128179a2d8af218faeaea2ac2ad1fd4b6debf4a
pojiro <pojiro.jp@gmail.com>
2022-07-14 17:19:52 +0900

    Rclex.NifsTest の後処理実装漏れを修正
    WHY: 後処理により解放すべきリソースを解放していないことが、テストの挙動が安定しないことがわかったから

--- test/rclex/nifs_test.exs
+++ test/rclex/nifs_test.exs
@@ -15,174 +15,199 @@ defmodule Rclex.NifsTest do
 
   alias Rclex.Nifs
 
-  describe "rcl_get_zero_initialized_init_options/0" do
-    test "return reference" do
-      assert is_reference(Nifs.rcl_get_zero_initialized_init_options())
+  describe "init_nif" do
+    test "rcl_get_zero_initialized_init_options/0 return reference" do
+      options = Nifs.rcl_get_zero_initialized_init_options()
+
+      try do
+        assert is_reference(options)
+      after
+        # clean up resource
+        Nifs.rcl_init_options_fini(options)
+      end
+    end
+
+    test "rcl_init_options_init/1 return :ok" do
+      options = Nifs.rcl_get_zero_initialized_init_options()
+
+      try do
+        assert :ok = Nifs.rcl_init_options_init(options)
+      after
+        # clean up resource
+        Nifs.rcl_init_options_fini(options)
+      end
+    end
+
+    test "rcl_init_options_init/1 raise ArgumentError" do
+      assert_raise ArgumentError, fn -> Nifs.rcl_init_options_init(make_ref()) end
+    end
+
+    test "rcl_init_options_fini/1 return reference" do
+      options = Nifs.rcl_get_zero_initialized_init_options()
+      :ok = Nifs.rcl_init_options_init(options)
+
+      assert is_reference(Nifs.rcl_init_options_fini(options))
+    end
+
+    # WHY: DO NOT SEPARATE THIS TESTS
+    # because rcl_shutdown expects context is initialized with options
+    test """
+    rcl_get_zero_initialized_context/0 return reference
+    rcl_init_with_null/2 return reference
+    """ do
+      options = Nifs.rcl_get_zero_initialized_init_options()
+      :ok = Nifs.rcl_init_options_init(options)
+      context = Nifs.rcl_get_zero_initialized_context()
+
+      try do
+        assert is_reference(context)
+        assert is_reference(Nifs.rcl_init_with_null(options, context))
+      after
+        # clean up resource
+        Nifs.rcl_init_options_fini(options)
+        Nifs.rcl_shutdown(context)
+      end
+    end
+
+    test "rcl_shutdown/1 return {:ok, reference}" do
+      context = get_initialized_context()
+
+      assert {:ok, rcl_ret} = Nifs.rcl_shutdown(context)
+      assert is_reference(rcl_ret)
     end
   end
 
-  describe "rcl_init_options_init/1" do
-    test "return :ok" do
-      assert :ok =
-               Nifs.rcl_get_zero_initialized_init_options()
-               |> Nifs.rcl_init_options_init()
+  describe "node_nif" do
+    setup do
+      context = get_initialized_context()
+      on_exit(fn -> Nifs.rcl_shutdown(context) end)
+      %{context: context}
     end
 
-    test "raise ArgumentError" do
-      assert_raise ArgumentError, fn ->
-        make_ref()
-        |> Nifs.rcl_init_options_init()
+    test "rcl_get_zero_initialized_node/0 return reference" do
+      node = Nifs.rcl_get_zero_initialized_node()
+
+      try do
+        assert is_reference(node)
+      after
+        # clean up resource
+        Nifs.rcl_node_fini(node)
+      end
+    end
+
+    test "rcl_node_get_default_options/0 return reference" do
+      assert is_reference(Nifs.rcl_node_get_default_options())
+
+      # TODO: clean up options, currently we don't have the function
+    end
+
+    test "rcl_node_init/5 return reference", %{context: context} do
+      node = Nifs.rcl_get_zero_initialized_node()
+      node_name = 'node'
+      node_namespace = 'namespace'
+      options = Nifs.rcl_node_get_default_options()
+
+      try do
+        assert is_reference(Nifs.rcl_node_init(node, node_name, node_namespace, context, options))
+      after
+        # TODO: clean up options, currently we don't have the function
+        Nifs.rcl_node_fini(node)
+      end
+    end
+
+    test "rcl_node_init_without_namespace/4 return reference", %{context: context} do
+      node_name = 'node'
+      node = Nifs.rcl_get_zero_initialized_node()
+      options = Nifs.rcl_node_get_default_options()
+
+      try do
+        assert is_reference(
+                 Nifs.rcl_node_init_without_namespace(node, node_name, context, options)
+               )
+      after
+        # TODO: clean up options, currently we don't have the function
+        Nifs.rcl_node_fini(node)
+      end
+    end
+
+    test "rcl_node_fini/1 return reference", %{context: context} do
+      node = get_initialized_no_namespace_node(context)
+
+      assert is_reference(Nifs.rcl_node_fini(node))
+    end
+
+    test "rcl_node_get_name/1 return charlist", %{context: context} do
+      node_name = 'test'
+      node = get_initialized_no_namespace_node(context, node_name)
+
+      try do
+        assert ^node_name = Nifs.rcl_node_get_name(node)
+      after
+        # clean up resource
+        Nifs.rcl_node_fini(node)
       end
     end
   end
 
-  describe "rcl_init_options_fini/1" do
-    test "return reference" do
-      options = Nifs.rcl_get_zero_initialized_init_options()
-      :ok = Nifs.rcl_init_options_init(options)
-      assert is_reference(Nifs.rcl_init_options_fini(options))
-    end
-  end
-
-  describe "rcl_get_zero_initialized_context/0" do
-    test "return reference" do
-      assert is_reference(Nifs.rcl_get_zero_initialized_context())
-    end
-  end
-
-  describe "rcl_init_with_null/2" do
-    test "return reference" do
-      init_options = Nifs.rcl_get_zero_initialized_init_options()
-      context = Nifs.rcl_get_zero_initialized_context()
-      assert is_reference(Nifs.rcl_init_with_null(init_options, context))
-    end
-  end
-
-  describe "rcl_shutdown/1" do
-    test "return {:ok, reference}" do
+  describe "publisher_nif" do
+    setup do
       context = get_initialized_context()
+      node = get_initialized_no_namespace_node(context)
 
-      assert {:ok, context} = Nifs.rcl_shutdown(context)
-      assert is_reference(context)
+      on_exit(fn ->
+        Nifs.rcl_shutdown(context)
+        Nifs.rcl_node_fini(node)
+      end)
+
+      %{node: node}
     end
-  end
 
-  describe "rcl_get_zero_initialized_node/0" do
-    test "return reference" do
-      assert is_reference(Nifs.rcl_get_zero_initialized_node())
-    end
-  end
-
-  describe "rcl_node_get_default_options/0" do
-    test "return reference" do
-      assert is_reference(Nifs.rcl_node_get_default_options())
-    end
-  end
-
-  describe "rcl_node_init/5" do
-    test "return reference" do
-      node = Nifs.rcl_get_zero_initialized_node()
-      node_name = 'node'
-      node_namespace = 'namespace'
-      context = get_initialized_context()
-      options = Nifs.rcl_node_get_default_options()
-
-      assert is_reference(Nifs.rcl_node_init(node, node_name, node_namespace, context, options))
-    end
-  end
-
-  describe "rcl_node_init_without_namespace/4" do
-    test "return reference" do
-      node = Nifs.rcl_get_zero_initialized_node()
-      node_name = 'node'
-      context = get_initialized_context()
-      options = Nifs.rcl_node_get_default_options()
-
-      assert is_reference(Nifs.rcl_node_init_without_namespace(node, node_name, context, options))
-    end
-  end
-
-  describe "rcl_node_fini/1" do
-    test "return reference" do
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
-
-      assert is_reference(Nifs.rcl_node_fini(node))
-    end
-  end
-
-  describe "rcl_node_get_name/1" do
-    test "return charlist" do
-      node_name = 'test'
-
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node(node_name)
-
-      assert ^node_name = Nifs.rcl_node_get_name(node)
-    end
-  end
-
-  describe "rcl_get_zero_initialized_publisher/0" do
-    test "return reference" do
+    test "rcl_get_zero_initialized_publisher/0 return reference" do
       assert is_reference(Nifs.rcl_get_zero_initialized_publisher())
+      # TODO: clean up publisher ?
     end
-  end
 
-  describe "rcl_publisher_get_default_options/0" do
-    test "return reference" do
+    test "rcl_publisher_get_default_options/0 return reference" do
       assert is_reference(Nifs.rcl_publisher_get_default_options())
+      # TODO: clean up options ?
     end
-  end
 
-  describe "rcl_publisher_fini/2" do
-    test "return reference" do
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
-
+    test "rcl_publisher_fini/2 return reference", %{node: node} do
       publihser = get_initialized_publisher(node)
 
       assert is_reference(Nifs.rcl_publisher_fini(publihser, node))
     end
-  end
 
-  describe "rcl_publisher_init/5" do
-    test "reutrn reference" do
+    test "rcl_publisher_init/5 return reference", %{node: node} do
       publisher = Nifs.rcl_get_zero_initialized_publisher()
-
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
 
       topic = 'topic'
       typesupport = Rclex.Msg.typesupport('StdMsgs.Msg.String')
       publisher_options = Nifs.rcl_publisher_get_default_options()
 
-      assert is_reference(
-               Nifs.rcl_publisher_init(publisher, node, topic, typesupport, publisher_options)
-             )
+      try do
+        assert is_reference(
+                 Nifs.rcl_publisher_init(publisher, node, topic, typesupport, publisher_options)
+               )
+      after
+        # clean up resource
+        Nifs.rcl_publisher_fini(publisher, node)
+      end
     end
-  end
 
-  describe "rcl_publisher_is_valid/1" do
-    test "return true" do
-      publisher =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
-        |> get_initialized_publisher()
+    test "rcl_publisher_is_valid/1 return true", %{node: node} do
+      publisher = get_initialized_publisher(node)
 
-      assert true = Nifs.rcl_publisher_is_valid(publisher)
+      try do
+        assert true = Nifs.rcl_publisher_is_valid(publisher)
+      after
+        # clean up resource
+        Nifs.rcl_publisher_fini(publisher, node)
+      end
     end
-  end
 
-  describe "rcl_publish/3" do
-    test "return rcl_ret_ok tuple" do
-      publisher =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
-        |> get_initialized_publisher()
+    test "rcl_publish/3 return rcl_ret_ok tuple", %{node: node} do
+      publisher = get_initialized_publisher(node)
 
       publisher_allocation = Nifs.create_pub_alloc()
 
@@ -197,95 +222,92 @@ defmodule Rclex.NifsTest do
 
       rcl_ret_ok = Rclex.ReturnCode.rcl_ret_ok()
 
-      assert {^rcl_ret_ok, publisher, publisher_allocation} =
-               Nifs.rcl_publish(publisher, message, publisher_allocation)
+      try do
+        assert {^rcl_ret_ok, publisher, publisher_allocation} =
+                 Nifs.rcl_publish(publisher, message, publisher_allocation)
 
-      assert is_reference(publisher)
-      assert is_reference(publisher_allocation)
+        assert is_reference(publisher)
+        assert is_reference(publisher_allocation)
+      after
+        # clean up resource
+        Nifs.rcl_publisher_fini(publisher, node)
+      end
     end
   end
 
-  describe "create_pub_alloc/0" do
-    test "return reference" do
+  describe "subscription_nif" do
+    setup do
+      context = get_initialized_context()
+      node = get_initialized_no_namespace_node(context)
+
+      on_exit(fn ->
+        Nifs.rcl_shutdown(context)
+        Nifs.rcl_node_fini(node)
+      end)
+
+      %{node: node}
+    end
+
+    test "create_pub_alloc/0 return reference" do
       assert is_reference(Nifs.create_pub_alloc())
     end
-  end
 
-  describe "rcl_subscription_get_default_options/0" do
-    test "return reference" do
+    test "rcl_subscription_get_default_options/0 return reference" do
       assert is_reference(Nifs.rcl_subscription_get_default_options())
     end
-  end
 
-  describe "rcl_get_zero_initialized_subscription/0" do
-    test "return reference" do
+    test "rcl_get_zero_initialized_subscription/0 return reference" do
       assert is_reference(Nifs.rcl_get_zero_initialized_subscription())
     end
-  end
 
-  describe "create_sub_alloc/0" do
-    test "return reference" do
+    test "create_sub_alloc/0 return reference" do
       assert is_reference(Nifs.create_sub_alloc())
     end
-  end
 
-  describe "rcl_subscription_init/5" do
-    test "return reference" do
+    test "rcl_subscription_init/5 return reference", %{node: node} do
       subscription = Nifs.rcl_get_zero_initialized_subscription()
-
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
 
       topic = 'topic'
       typesupport = Rclex.Msg.typesupport('StdMsgs.Msg.String')
       subscription_options = Nifs.rcl_subscription_get_default_options()
 
-      assert is_reference(
-               Nifs.rcl_subscription_init(
-                 subscription,
-                 node,
-                 topic,
-                 typesupport,
-                 subscription_options
+      try do
+        assert is_reference(
+                 Nifs.rcl_subscription_init(
+                   subscription,
+                   node,
+                   topic,
+                   typesupport,
+                   subscription_options
+                 )
                )
-             )
+      after
+        # clean up resource
+        Nifs.rcl_subscription_fini(subscription, node)
+      end
     end
-  end
 
-  describe "rcl_subscription_fini/2" do
-    test "return reference" do
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
-
+    test "rcl_subscription_fini/2 return reference", %{node: node} do
       subscription = get_initialized_subscription(node)
 
       assert is_reference(Nifs.rcl_subscription_fini(subscription, node))
     end
-  end
 
-  describe "rcl_subscription_get_topic_name/1" do
-    test "return charlist" do
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
-
+    test "rcl_subscription_get_topic_name/1 return charlist", %{node: node} do
       topic_name = 'test'
       subscription = get_initialized_subscription(node, topic_name)
 
-      # TODO: get_topic_name が '/' 含んで返すことが正か仕様の確認
-      [_slash | topic] = Nifs.rcl_subscription_get_topic_name(subscription)
-      assert ^topic_name = topic
+      try do
+        # TODO: get_topic_name が '/' 含んで返すことが正か仕様の確認
+        [_slash | topic] = Nifs.rcl_subscription_get_topic_name(subscription)
+        assert ^topic_name = topic
+      after
+        # clean up resource
+        Nifs.rcl_subscription_fini(subscription, node)
+      end
     end
-  end
 
-  describe "rcl_take/4" do
-    test "return tuple" do
-      node =
-        get_initialized_context()
-        |> get_initialized_no_namespace_node()
-
+    test "rcl_take/4 return tuple", %{node: node} do
       subscription = get_initialized_subscription(node)
 
       msg = Rclex.Msg.initialize('StdMsgs.Msg.String')
@@ -294,12 +316,17 @@ defmodule Rclex.NifsTest do
 
       rcl_ret_subscription_take_failed = Rclex.ReturnCode.rcl_ret_subscription_take_failed()
 
-      assert {^rcl_ret_subscription_take_failed, subscription, msginfo, subscription_allocation} =
-               Nifs.rcl_take(subscription, msg, msginfo, subscription_allocation)
+      try do
+        assert {^rcl_ret_subscription_take_failed, subscription, msginfo, subscription_allocation} =
+                 Nifs.rcl_take(subscription, msg, msginfo, subscription_allocation)
 
-      assert is_reference(subscription)
-      assert is_reference(msginfo)
-      assert is_reference(subscription_allocation)
+        assert is_reference(subscription)
+        assert is_reference(msginfo)
+        assert is_reference(subscription_allocation)
+      after
+        # clean up resource
+        Nifs.rcl_subscription_fini(subscription, node)
+      end
     end
   end
 end
