5132f8c508fe331cb0a94f397aad92a3e3033e00
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-10-06 12:55:48 +0900

    Add `mix rclex.ros2` task

---
+++ lib/mix/tasks/rclex/ros2.ex
@@ -0,0 +1,109 @@
+defmodule Mix.Tasks.Rclex.Ros2 do
+  use Mix.Task
+
+  @arm64v8_ros2_distros ["foxy", "galactic", "humble"]
+  @supported_arch ["arm64v8"]
+  @supported_ros2_distros %{"arm64v8" => @arm64v8_ros2_distros}
+
+  @shortdoc "Prepare ROS2 resources under .ros2 directory."
+  @moduledoc """
+  #{@shortdoc}
+
+      mix rclex.ros2 --arch ARCH --ros2-distro DISTRO
+
+  ROS2 resources will be prepared under .ros2.
+
+  An `--arch` option should be specified, option value is `arm64v8`, currently only supported.
+
+  A `--ros2-distro` option should be specified, option values are #{Enum.join(@arm64v8_ros2_distros, "/")}.
+
+  ## Examples
+
+      mix rclex.ros2 --arch arm64v8 --ros2-distro foxy
+  """
+
+  @switches [arch: :string, nerves_system: :string, ros2_distro: :string]
+
+  def run(args) do
+    if not command_exists?("docker") do
+      Mix.raise("""
+      Please install docker command first, we need it.
+      """)
+    end
+
+    parsed_args = parse_args(args)
+
+    arch = Keyword.get(parsed_args, :arch)
+
+    if arch not in @supported_arch do
+      Mix.raise("""
+      Please select and specify the appropriate arch from the following.
+      #{Enum.join(@supported_arch, ", ")}
+      """)
+    end
+
+    supported_ros2_distros = Map.get(@supported_ros2_distros, arch, [])
+    ros2_distro = Keyword.get(parsed_args, :ros2_distro)
+
+    if ros2_distro not in supported_ros2_distros do
+      Mix.raise("""
+      Please select and specify the appropriate ros2 distro from the following.
+      #{Enum.join(supported_ros2_distros, ", ")}
+      """)
+    end
+
+    copy_ros2_resources!(File.cwd!(), arch, ros2_distro)
+  end
+
+  def command_exists?(command) when is_binary(command) do
+    {_, exit_status} = System.cmd("sh", ["-c", "command -v #{command}"])
+    exit_status == 0
+  end
+
+  def copy_ros2_resources!(dest_base_path, arch, ros2_distro) do
+    directory_path = create_resources_directory!(dest_base_path, arch, ros2_distro)
+    File.write!(Path.join(directory_path, ".gitignore"), "*")
+
+    copy_ros2_resources_impl!(directory_path, arch, ros2_distro)
+  end
+
+  def copy_ros2_resources_impl!(dest_path, arch, ros2_distro) do
+    tag = ros2_docker_image_tag(arch, ros2_distro)
+
+    ["/opt/ros/#{ros2_distro}/include", "/opt/ros/#{ros2_distro}/lib"]
+    |> Enum.map(&copy_ros2_impl!(tag, &1, dest_path))
+  end
+
+  def copy_ros2_impl!(docker_tag, src_path, dest_path) do
+    with true <- File.exists?(src_path),
+         true <- File.exists?(dest_path) do
+      docker_command_args = ["run", "--rm", "-v", "#{dest_path}:/root", docker_tag]
+      copy_command = ["cp", "-rf", src_path, "/root"]
+
+      {_, 0} = System.cmd("docker", docker_command_args ++ copy_command)
+    end
+  end
+
+  def ros2_docker_image_tag("arm64v8", ros2_distro) when ros2_distro in @arm64v8_ros2_distros do
+    "arm64v8/ros:#{ros2_distro}-ros-core"
+  end
+
+  def parse_args(args) do
+    {parsed_args, _remaining_args, _invalid} = OptionParser.parse(args, strict: @switches)
+
+    parsed_args
+  end
+
+  @spec create_resources_directory!(
+          base_path :: String.t(),
+          arch :: String.t(),
+          ros2_distro :: String.t()
+        ) ::
+          directory_path :: String.t()
+  def create_resources_directory!(base_path, arch, ros2_distro) do
+    directory_path = Path.join(base_path, ".ros2/resources/from-docker/#{arch}/#{ros2_distro}")
+    File.mkdir_p!(directory_path)
+
+    directory_path
+  end
+end
---
+++ test/mix/tasks/rclex/ros2_test.exs
@@ -0,0 +1,54 @@
+defmodule Mix.Tasks.Rclex.Ros2Test do
+  use ExUnit.Case
+
+  test "command_exists?/1" do
+    assert Mix.Tasks.Rclex.Ros2.command_exists?("ls")
+    refute Mix.Tasks.Rclex.Ros2.command_exists?("non_existent_command")
+  end
+
+  @tag skip: "take time"
+  @tag :tmp_dir
+  test "copy_ros2_resources!/3", %{tmp_dir: tmp_dir_path} do
+    Mix.Tasks.Rclex.Ros2.copy_ros2_resources!(tmp_dir_path, "arm64v8", "foxy")
+
+    assert File.ls!(tmp_dir_path) |> Enum.count() > 0
+  end
+
+  @tag skip: "take time"
+  @tag :tmp_dir
+  test "copy_ros2_impl!/1", %{tmp_dir: tmp_dir_path} do
+    Mix.Tasks.Rclex.Ros2.copy_ros2_impl!(
+      "arm64v8/ros:foxy-ros-core",
+      "/opt/ros/foxy/setup.sh",
+      tmp_dir_path
+    )
+
+    assert File.ls!(tmp_dir_path) |> Enum.count() > 0
+  end
+
+  test "ros2_docker_image_tag/2" do
+    assert "arm64v8/ros:foxy-ros-core" =
+             Mix.Tasks.Rclex.Ros2.ros2_docker_image_tag("arm64v8", "foxy")
+  end
+
+  test "parse_args/1" do
+    assert [arch: "arm64v8", ros2_distro: "foxy"] =
+             Mix.Tasks.Rclex.Ros2.parse_args(["--arch", "arm64v8", "--ros2-distro", "foxy"])
+
+    assert [nerves_system: "rpi4", ros2_distro: "foxy"] =
+             Mix.Tasks.Rclex.Ros2.parse_args(["--nerves-system", "rpi4", "--ros2-distro", "foxy"])
+  end
+
+  @tag :tmp_dir
+  test "create_resources_directory!/2", %{tmp_dir: tmp_dir_path} do
+    arch = "arm64v8"
+    ros2_distro = "foxy"
+
+    directory_path = Path.join(tmp_dir_path, ".ros2/resources/from-docker/#{arch}/#{ros2_distro}")
+
+    ^directory_path =
+      Mix.Tasks.Rclex.Ros2.create_resources_directory!(tmp_dir_path, arch, ros2_distro)
+
+    assert File.exists?(directory_path)
+  end
+end
