ce7c332f9bea45fac473fc86b12668f8e0cb3ede
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-10-25 07:37:14 +0900

    Remove unused message type from config.exs

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,7 @@ import Config
 config :rclex,
   ros2_message_types: [
     "std_msgs/msg/String",
-    "geometry_msgs/msg/Twist",
-    "geometry_msgs/msg/TwistWithCovariance"
+    "geometry_msgs/msg/Twist"
   ]
 
 import_config "#{config_env()}.exs"
