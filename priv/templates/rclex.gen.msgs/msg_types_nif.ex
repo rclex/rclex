8eec7ad266fb4a84cb122c860eb987581fd9872c
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-10-25 08:10:48 +0900

    ファイルの存在意義が分かるようコメントを追加

--- priv/templates/rclex.gen.msgs/msg_types_nif.ex
+++ priv/templates/rclex.gen.msgs/msg_types_nif.ex
@@ -2,7 +2,8 @@
 defmodule Rclex.MsgTypesNif do
   defmacro __before_compile__(_env) do
     quote do
-
+      # This file is necessary for `mix compile` which is invoked
+      # before `mix rclex.gen.msgs`
     end
   end
 end
