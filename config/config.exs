8e654bd949df154e999bee27b032e76db036f987
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-10-03 10:08:55 +0900

    Reset rclex

--- config/config.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :rclex,
-  ros2_message_types: [
-    "std_msgs/msg/String",
-    "geometry_msgs/msg/Twist"
-  ]
-
-import_config "#{config_env()}.exs"
--- priv/templates/rclex.gen.msgs/lib/rclex/msg_types_nif.ex
+++
@@ -1,9 +0,0 @@
-# credo:disable-for-this-file
-defmodule Rclex.MsgTypesNif do
-  defmacro __before_compile__(_env) do
-    quote do
-      # This file is necessary for `mix compile` which is invoked
-      # before `mix rclex.gen.msgs`
-    end
-  end
-end
