67a6358619ccca9e0f40b59b12759e7a4b538789
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-10-12 10:02:48 +0900

    Add to_tuple, to_struct callback to message_behaviour.ex

--- lib/rclex/message_behaviour.ex
+++ lib/rclex/message_behaviour.ex
@@ -6,4 +6,6 @@ defmodule Rclex.MessageBehaviour do
   @callback destroy!(message :: reference()) :: :ok
   @callback set!(message :: reference(), data :: any()) :: :ok
   @callback get!(message :: reference()) :: data :: any()
+  @callback to_tuple(struct()) :: tuple()
+  @callback to_struct(tuple()) :: struct()
 end
