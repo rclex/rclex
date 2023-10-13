7534d12e479e94fb2f3b3e9e997ea4cf1ad6bea6
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-10-13 17:11:30 +0900

    Move generate_msg_ex/2 to Generators.MsgH.generate/2

---
+++ test/rclex/generators/msg_h_test.exs
@@ -0,0 +1,34 @@
+defmodule Rclex.Generators.MsgHTest do
+  use ExUnit.Case
+
+  with ros_distro <- System.get_env("ROS_DISTRO"),
+       true <- File.exists?("/opt/ros/#{ros_distro}") do
+    @ros_share_path "/opt/ros/#{ros_distro}/share"
+  else
+    _ ->
+      @moduletag :skip
+  end
+
+  alias Mix.Tasks.Rclex.Gen.Msgs
+  alias Rclex.Generators.MsgH
+  alias Rclex.Generators.Util
+
+  for ros2_message_type <- [
+        "std_msgs/msg/String",
+        "std_msgs/msg/MultiArrayLayout",
+        "std_msgs/msg/UInt32MultiArray",
+        "geometry_msgs/msg/Vector3",
+        "geometry_msgs/msg/Twist"
+      ] do
+    test "generate/2 #{ros2_message_type}" do
+      ros2_message_type = unquote(ros2_message_type)
+      ros2_message_type_map = Msgs.get_ros2_message_type_map(ros2_message_type, @ros_share_path)
+
+      [interfaces, msg, type] = String.split(ros2_message_type, "/")
+      type_path = Enum.join([interfaces, msg, Util.to_down_snake(type)], "/")
+
+      assert MsgH.generate(ros2_message_type, ros2_message_type_map) ==
+               File.read!(Path.join(File.cwd!(), "test/expected_files/#{type_path}.h"))
+    end
+  end
+end
