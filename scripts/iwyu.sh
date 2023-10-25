b631a57170288640d671ff1b8e8fec5a110542b8
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-10-25 14:10:47 +0900

    Refactor codes relates to iwyu

--- scripts/iwyu.sh
+++ scripts/iwyu.sh
@@ -4,7 +4,12 @@
 
 include-what-you-use \
 -I"$HOME/.asdf/installs/erlang/26.0.2/usr/include" \
--I"/opt/ros/$ROS_DISTRO/include" \
+-I"/opt/ros/$ROS_DISTRO/include/rcl" \
+-I"/opt/ros/$ROS_DISTRO/include/rcutils" \
+-I"/opt/ros/$ROS_DISTRO/include/rmw" \
+-I"/opt/ros/$ROS_DISTRO/include/rcl_yaml_param_parser" \
+-I"/opt/ros/$ROS_DISTRO/include/rosidl_runtime_c" \
+-I"/opt/ros/$ROS_DISTRO/include/rosidl_typesupport_interface" \
 -Xiwyu --max_line_length=100 \
 -Xiwyu --quoted_includes_first \
 -Xiwyu --no_comments \
