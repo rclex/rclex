cf48db7477a28be7010e1b2e48ba36ab5ef17524
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-10-25 11:53:01 +0900

    Impl rclex.gen.msgs

---
+++ config/config.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :rclex,
+  ros2_message_types: [
+    "std_msgs/msg/String",
+    "std_msgs/msg/UInt32MultiArray",
+    "geometry_msgs/msg/Twist",
+    "sensor_msgs/msg/PointCloud"
+  ]
