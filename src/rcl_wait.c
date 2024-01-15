35ccfa4705dc5e331a210a54b53a7c56c55d7da1
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-01-15 09:19:55 +0900

    Refine nif code which relates to nif allocator

--- src/rcl_wait.c
+++ src/rcl_wait.c
@@ -21,9 +21,10 @@ ERL_NIF_TERM nif_rcl_wait_set_init_subscription(ErlNifEnv *env, int argc,
   if (!rcl_context_is_valid(context_p)) return raise(env, __FILE__, __LINE__);
 
   rcl_ret_t rc;
+  rcl_wait_set_t wait_set   = rcl_get_zero_initialized_wait_set();
+  rcl_allocator_t allocator = get_nif_allocator();
 
-  rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-  rc = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, context_p, rcl_get_default_allocator());
+  rc = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, context_p, allocator);
   if (rc != RCL_RET_OK) return raise(env, __FILE__, __LINE__);
 
   rcl_wait_set_t *obj = enif_alloc_resource(rt_rcl_wait_set_t, sizeof(rcl_wait_set_t));
@@ -43,9 +44,10 @@ ERL_NIF_TERM nif_rcl_wait_set_init_timer(ErlNifEnv *env, int argc, const ERL_NIF
   if (!rcl_context_is_valid(context_p)) return raise(env, __FILE__, __LINE__);
 
   rcl_ret_t rc;
+  rcl_wait_set_t wait_set   = rcl_get_zero_initialized_wait_set();
+  rcl_allocator_t allocator = get_nif_allocator();
 
-  rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-  rc = rcl_wait_set_init(&wait_set, 0, 0, 1, 0, 0, 0, context_p, get_nif_allocator());
+  rc = rcl_wait_set_init(&wait_set, 0, 0, 1, 0, 0, 0, context_p, allocator);
   if (rc != RCL_RET_OK) return raise(env, __FILE__, __LINE__);
 
   rcl_wait_set_t *obj = enif_alloc_resource(rt_rcl_wait_set_t, sizeof(rcl_wait_set_t));
