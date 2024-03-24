6a04537c10173b45c7942b42354f7c97361eae02
Penzlin, Felix <felix.penzlin@hilti.com>
2024-03-24 16:59:06 +0100

    remove external dependencies

--- config/config.exs
+++ config/config.exs
@@ -2,9 +2,6 @@ import Config
 
 config :rclex,
   ros2_message_types: [
-    "raspimouse_msgs/msg/Leds",
-    "raspimouse_msgs/msg/LightSensors",
-    "raspimouse_msgs/msg/Switches",
     "std_msgs/msg/String",
     "std_msgs/msg/UInt32MultiArray",
     "geometry_msgs/msg/Twist",
