ffcaace8a575a3eee0346c41c48ee29bf1557351
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2026-06-08 18:18:41 +0900

    fix: remove unreachable code in child termination and message publishing
    Warning in `mix compile` with Elixir 1.20
    ```
    make: Nothing to be done for 'all'.
        warning: the following clause will never match:
    
            {_atom, _node} ->
    
        because it attempts to match on the result of:
    
            GenServer.whereis(name(message_type, topic_name, name, namespace))
    
        which has type:
    
            dynamic(nil or pid())
    
        type warning found at:
        │
     26 │       {_atom, _node} -> raise("should not happen")
        │       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        │
        └─ lib/rclex/publisher.ex:26: Rclex.Publisher.publish/4
    
        warning: the following clause will never match:
    
            {_atom, _node} ->
    
        because it attempts to match on the result of:
    
            GenServer.whereis(name)
    
        which has type:
    
            dynamic(nil or pid())
    
        type warning found at:
        │
     26 │       {_atom, _node} -> raise("should not happen")
        │       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        │
        └─ lib/rclex/nodes_supervisor.ex:26: Rclex.NodesSupervisor.terminate_child/2
    
    ```

--- lib/rclex/publisher.ex
+++ lib/rclex/publisher.ex
@@ -23,7 +23,6 @@ defmodule Rclex.Publisher do
   def publish(%message_type{} = message, topic_name, name, namespace \\ "/") do
     case GenServer.whereis(name(message_type, topic_name, name, namespace)) do
       nil -> {:error, :not_found}
-      {_atom, _node} -> raise("should not happen")
       pid -> GenServer.call(pid, {:publish, message})
     end
   end
