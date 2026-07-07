bcc11751e157f59d79d1f179c8c1cca8cedbfcbd
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-07-07 14:23:17 +0900

    Add `--dockerfile` option concept to mix rclex.prep.ros2

--- lib/mix/tasks/rclex/prep/ros2.ex
+++ lib/mix/tasks/rclex/prep/ros2.ex
@@ -11,6 +11,10 @@ defmodule Mix.Tasks.Rclex.Prep.Ros2 do
 
   An `--arch` option should be specified. Supported values are `arm64v8`, `amd64`, and `arm32v7`.
 
+  You can also customize the copy source image with one of the following options (supported for `arm64v8` and `amd64` only).
+
+  - `--dockerfile PATH`: build an arbitrary Dockerfile and copy resources from that image.
+
   ## Examples
 
   specify arch explicitly with `--arch` option
@@ -24,6 +28,24 @@ defmodule Mix.Tasks.Rclex.Prep.Ros2 do
   ```
   mix rclex.prep.ros2
   ```
+
+  Build a custom image from an arbitrary Dockerfile.
+
+  ```
+  mix rclex.prep.ros2 --arch arm64v8 --dockerfile docker/ros2/Dockerfile
+  ```
+
+  Example Dockerfile for `rmw_zenoh_cpp` (ROS 2 Jazzy)
+
+  ```
+  FROM arm64v8/ros:jazzy-ros-core
+
+  RUN apt-get update \\
+      && apt-get install -y --no-install-recommends \\
+           ros-jazzy-rmw-zenoh-cpp \\
+      && rm -rf /var/lib/apt/lists/*
+  ```
+
   """
 
   use Mix.Task
@@ -37,10 +59,11 @@ defmodule Mix.Tasks.Rclex.Prep.Ros2 do
     "arm32v7" => @arm32v7_ros_distros
   }
   @supported_arch Map.keys(@supported_ros_distros)
+  @supported_custom_docker_arch ["arm64v8", "amd64"]
 
   @nerves_target_arch_map %{"rpi4" => "arm64v8", "rpi3" => "arm32v7"}
 
-  @switches [arch: :string]
+  @switches [arch: :string, dockerfile: :string]
 
   @doc false
   def run(args) do
@@ -76,64 +99,84 @@ defmodule Mix.Tasks.Rclex.Prep.Ros2 do
       """)
     end
 
+    docker_input_mode = docker_input_mode(parsed_args)
+    validate_custom_docker_mode!(arch, docker_input_mode)
+    docker_tag = resolve_copy_source_image!(arch, ros_distro, docker_input_mode)
+
     # NOTE: If you implement the copy destination option, pass the path here.
     dest_dir_path = copy_dest_dir_path(arch, ros_distro)
 
     message = """
     Are you sure to copy ROS 2 resources to following directory?
     path: #{dest_dir_path}
+    image: #{docker_tag}
     """
 
     if Mix.shell().yes?(String.trim_trailing(message)) do
-      copy_from_docker!(dest_dir_path, arch, ros_distro)
+      copy_from_docker!(dest_dir_path, arch, ros_distro, docker_tag)
     end
   end
 
   @doc false
   def parse_args(args) do
-    {parsed_args, _remaining_args, _invalid} = OptionParser.parse(args, strict: @switches)
+    {parsed_args, _remaining_args, invalid} = OptionParser.parse(args, strict: @switches)
+
+    if invalid != [] do
+      invalid_options = Enum.map_join(invalid, ", ", fn {key, _value} -> to_string(key) end)
+      Mix.raise("Unknown options: #{invalid_options}")
+    end
 
     parsed_args
   end
 
   @doc false
-  def copy_from_docker!(dest_dir_path, arch, ros_distro) do
-    docker_tag = ros_docker_image_tag(arch, ros_distro)
+  @spec docker_input_mode(keyword()) :: :default | {:dockerfile, String.t()}
+  def docker_input_mode(parsed_args) do
+    case Keyword.get(parsed_args, :dockerfile) do
+      nil -> :default
+      dockerfile -> {:dockerfile, dockerfile}
+    end
+  end
+
+  @doc false
+  def copy_from_docker!(dest_dir_path, arch, ros_distro, docker_tag) do
     Mix.shell().info("\nCopy from image: #{docker_tag}\n")
 
     dest_path = Path.join(dest_dir_path, "/opt/ros/#{ros_distro}")
     create_resources_directory!(dest_path, _git_ignore = true)
-    copy_ros_resources_from_docker!(dest_path, arch, ros_distro)
+    copy_ros_resources_from_docker!(dest_path, arch, ros_distro, docker_tag)
 
     dest_path = Path.join(dest_dir_path, "/opt/ros/#{ros_distro}/lib")
     create_resources_directory!(dest_path, _git_ignore = false)
-    copy_vendor_resources_from_docker!(dest_path, arch, ros_distro)
+    copy_vendor_resources_from_docker!(dest_path, arch, ros_distro, docker_tag)
   end
 
-  defp copy_ros_resources_from_docker!(dest_path, arch, ros_distro)
+  defp copy_ros_resources_from_docker!(dest_path, arch, ros_distro, docker_tag)
        when arch in ["arm64v8", "amd64"] do
     [
       "/opt/ros/#{ros_distro}/include",
       "/opt/ros/#{ros_distro}/lib",
-      "/opt/ros/#{ros_distro}/share"
+      "/opt/ros/#{ros_distro}/share",
+      # vendor's ex.) zenoh_cpp_vendor
+      "/opt/ros/#{ros_distro}/opt"
     ]
-    |> Enum.map(fn src_path -> copy_from_docker_impl!(arch, ros_distro, src_path, dest_path) end)
+    |> Enum.map(fn src_path -> copy_from_docker_impl!(docker_tag, arch, src_path, dest_path) end)
   end
 
-  defp copy_ros_resources_from_docker!(dest_path, arch, ros_distro)
+  defp copy_ros_resources_from_docker!(dest_path, arch, _ros_distro, docker_tag)
        when arch in ["arm32v7"] do
     [
       "/root/ros2_ws/install/*/include",
       "/root/ros2_ws/install/*/lib",
       "/root/ros2_ws/install/*/share"
     ]
-    |> Enum.map(fn src_path -> copy_from_docker_impl!(arch, ros_distro, src_path, dest_path) end)
+    |> Enum.map(fn src_path -> copy_from_docker_impl!(docker_tag, arch, src_path, dest_path) end)
   end
 
-  defp copy_vendor_resources_from_docker!(dest_path, arch, ros_distro)
+  defp copy_vendor_resources_from_docker!(dest_path, arch, ros_distro, docker_tag)
        when arch in ["arm64v8", "amd64", "arm32v7"] do
     vendor_resources(arch, ros_distro)
-    |> Enum.map(fn src_path -> copy_from_docker_impl!(arch, ros_distro, src_path, dest_path) end)
+    |> Enum.map(fn src_path -> copy_from_docker_impl!(docker_tag, arch, src_path, dest_path) end)
   end
 
   defp vendor_resources(arch, "humble") do
@@ -161,10 +204,8 @@ defmodule Mix.Tasks.Rclex.Prep.Ros2 do
     ]
   end
 
-  defp copy_from_docker_impl!(arch, ros_distro, src_path, dest_path) do
+  defp copy_from_docker_impl!(docker_tag, arch, src_path, dest_path) do
     with true <- File.exists?(dest_path) do
-      docker_tag = ros_docker_image_tag(arch, ros_distro)
-
       docker_command_args = [
         "run",
         "--rm",
@@ -212,6 +253,165 @@ defmodule Mix.Tasks.Rclex.Prep.Ros2 do
 
   def ros_docker_image_tag("arm32v7", ros_distro) when ros_distro in @arm32v7_ros_distros do
     "rclex/arm32v7_ros_docker_with_vendor_resources:#{ros_distro}"
+  end
+
+  defp resolve_copy_source_image!(arch, ros_distro, :default) do
+    ros_docker_image_tag(arch, ros_distro)
+  end
+
+  defp resolve_copy_source_image!(arch, ros_distro, {:dockerfile, input_path}) do
+    prepare_custom_docker_image!(arch, ros_distro, input_path)
+  end
+
+  defp validate_custom_docker_mode!(_arch, :default) do
+    :ok
+  end
+
+  defp validate_custom_docker_mode!(arch, {:dockerfile, _path})
+       when arch in @supported_custom_docker_arch do
+    :ok
+  end
+
+  defp validate_custom_docker_mode!(arch, {:dockerfile, _path}) do
+    Mix.raise(
+      "Custom Dockerfile support is available only for #{Enum.join(@supported_custom_docker_arch, ", ")}. Received: #{arch}"
+    )
+  end
+
+  defp prepare_custom_docker_image!(arch, ros_distro, input_path) do
+    input_path = validate_input_file!(input_path)
+    spec = custom_docker_build_spec!(arch, ros_distro, input_path)
+
+    build_custom_docker_image!(spec)
+    validate_custom_docker_image!(spec)
+
+    spec.image_tag
+  end
+
+  defp validate_input_file!(input_path) do
+    expanded_path = Path.expand(input_path)
+
+    cond do
+      not File.exists?(expanded_path) ->
+        Mix.raise("--dockerfile file does not exist: #{expanded_path}")
+
+      not File.regular?(expanded_path) ->
+        Mix.raise("--dockerfile path is not a regular file: #{expanded_path}")
+
+      true ->
+        expanded_path
+    end
+  end
+
+  defp custom_docker_build_spec!(arch, ros_distro, input_path) do
+    # https://docs.docker.com/build/concepts/context/#what-is-a-build-context
+    context_path = Path.dirname(input_path)
+
+    %{
+      arch: arch,
+      ros_distro: ros_distro,
+      image_tag: "rclex/prep_ros2:#{arch}-#{ros_distro}",
+      source_path: input_path,
+      build_context_path: context_path,
+      build_dockerfile_path: input_path
+    }
+  end
+
+  defp build_custom_docker_image!(spec) do
+    Mix.shell().info("\nBuilding image: #{spec.image_tag}\n")
+
+    build_args = [
+      "build",
+      "--progress=plain",
+      "--platform",
+      platform(spec.arch),
+      "-t",
+      spec.image_tag,
+      "-f",
+      spec.build_dockerfile_path,
+      spec.build_context_path
+    ]
+
+    {command_output, status} = run_command_with_live_output!("docker", build_args)
+
+    if status != 0 do
+      Mix.raise("""
+      Failed to build Docker image.
+      mode: dockerfile
+      dockerfile: #{spec.build_dockerfile_path}
+      context: #{spec.build_context_path}
+      image: #{spec.image_tag}
+      exit_status: #{status}
+      output:
+      #{command_output}
+      """)
+    end
+  end
+
+  defp validate_custom_docker_image!(spec) do
+    validation_paths = ros_resource_validation_paths(spec.arch, spec.ros_distro)
+    validation_command = Enum.map_join(validation_paths, " && ", &"test -d #{&1}")
+
+    validate_args = [
+      "run",
+      "--rm",
+      "--platform",
+      platform(spec.arch),
+      spec.image_tag,
+      "bash",
+      "-c",
+      validation_command
+    ]
+
+    {command_output, status} = run_command_with_live_output!("docker", validate_args)
+
+    if status != 0 do
+      Mix.raise("""
+      Built image does not contain the required ROS 2 paths.
+      image: #{spec.image_tag}
+      required_paths: #{Enum.join(validation_paths, ", ")}
+      exit_status: #{status}
+      output:
+      #{command_output}
+      """)
+    end
+  end
+
+  defp ros_resource_validation_paths(arch, ros_distro)
+       when arch in @supported_custom_docker_arch do
+    [
+      "/opt/ros/#{ros_distro}/include",
+      "/opt/ros/#{ros_distro}/lib",
+      "/opt/ros/#{ros_distro}/share"
+    ]
+  end
+
+  defp run_command_with_live_output!(command, args) do
+    executable =
+      System.find_executable(command) ||
+        Mix.raise("Failed to find executable for command: #{command}")
+
+    port =
+      Port.open({:spawn_executable, executable}, [
+        :binary,
+        :exit_status,
+        :use_stdio,
+        :stderr_to_stdout,
+        args: args
+      ])
+
+    collect_port_output(port, "")
+  end
+
+  defp collect_port_output(port, acc) do
+    receive do
+      {^port, {:data, data}} ->
+        Mix.shell().info(data)
+        collect_port_output(port, acc <> data)
+
+      {^port, {:exit_status, status}} ->
+        {acc, status}
+    end
   end
 
   defp arch_dir_name("amd64"), do: "x86_64-linux-gnu"
